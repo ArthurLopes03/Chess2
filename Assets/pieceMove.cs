4f8bddc025b618936c5af1784972c3f09e60feaf
Seatwentytwo <123640807+Seatwentytwo@users.noreply.github.com>
2024-09-21 21:45:36 +0100

    Pawns
    Got pawns working in my scene

--- Assets/pieceMove.cs
+++ Assets/pieceMove.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class pieceMove : MonoBehaviour
 {
+    public bool isWhite;
+    [SerializeField] private bool firstMove;
+    Transform position;
+    Vector3 currentPosition;
+
+    private int moveDist;
+    private int movedistDouble;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveDist = 2;
+        movedistDouble = 4;
+        position = GetComponent<Transform>();
+    }
+
+    private void OnMouseDown()
+    {
+        if(isWhite == true)
+        {
+            transform.position = transform.position + new Vector3(0, moveDist, 0);
+        }
+        else
+        {
+            transform.position = transform.position - new Vector3(0, moveDist, 0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.position += new Vector3(1, 1, 1);
+
+        if (Input.GetKey("space"))
+        {
+            transform.position = transform.position + new Vector3(0, moveDist, 0);
+        }
     }
 }
