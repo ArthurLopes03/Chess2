0a7b8f706ac4292ef27b9c78548b341192573f88
ArthurLopes03 <C22509166@mytudublin.ie>
2024-09-21 14:50:16 +0100

    First push

---
+++ Assets/Grid.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public GameObject squarePrefab;
+
+    public Dictionary<Vector2, Square> squares;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        squares = new Dictionary<Vector2, Square>();
+        for (int x = 0; x < 8; x++)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                Square square = Instantiate(squarePrefab, new Vector3(x, y, 0), Quaternion.identity).GetComponent<Square>();
+
+                square.position = new Vector2(x+1, y+1);
+
+                squares.Add(square.position, square);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Square.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+    public Vector2 position;
+
+    public ChessPiece piece = null;
+}
